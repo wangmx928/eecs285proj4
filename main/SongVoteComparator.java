3260efef4e47b2d25e90dc0db79fff2745059c32
Lu Wang <luwang19891218@gmail.com>
2014-11-24 14:11:41 -0500

    modified comparator.

--- main/SongVoteComparator.java
+++ main/SongVoteComparator.java
@@ -6,6 +6,8 @@ public class SongVoteComparator implements Comparator<Song>{
 	public int compare(Song s1, Song s2) {
 		if(s1.getVoteCount() > s2.getVoteCount())
 			return 1;
+		else if(s1.getVoteCount() == s2.getVoteCount())
+			return return (s1.getSongTitle().compareTo(s2.getSongTitle()));
 		else return -1;
 	}
 }
