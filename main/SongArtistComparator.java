3d3a588de533832bbcc86ed0480c80aa15145ed4
Lu Wang <luwang19891218@gmail.com>
2014-11-24 14:09:50 -0500

    modified comparator

--- main/SongArtistComparator.java
+++ main/SongArtistComparator.java
@@ -4,6 +4,9 @@ import java.util.Comparator;
 
 public class SongArtistComparator implements Comparator<Song>{
 	public int compare(Song s1, Song s2) {
-		return (s1.getArtist().compareTo(s2.getArtist()));
+		if(!s1.getArtist().equals(s2.getArtist()))
+			return (s1.getArtist().compareTo(s2.getArtist()));
+		else
+			return (s1.getSongTitle().compareTo(s2.getSongTitle()));
 	}
 }
